17e0b5d036f2b03935e437a97cc51ad768a4485c
Bryan Josue Gonzalez Luna <gonzalezlunabj@hotmail.com>
2018-05-05 19:26:44 -0500

    Interface de Java
    Creación de constantes de programa para facilitar el mantenimiento del programa.

---
+++ src/compilador/Constantes.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compilador;
+
+/**
+ *
+ * @author Bryan Josue Gonzalez Luna
+ */
+public interface Constantes {
+    
+    public final int INDENTIFICADOR=0;
+    public final int ENTERO=1;
+    public final int REAL=2;
+    public final int CADENA=3;
+    public final int TIPO_DATO=4;
+    public final int OP_SUMA=5;
+    public final int OP_MUL=6;
+    public final int OP_REALC=7;
+    public final int OP_OR=8;
+    public final int OP_AND=9;
+    public final int OP_NOT=10;
+    public final int OP_IGUALDAD=11;
+    public final int PUNTO_Y_COMA=12;
+    public final int COMA=13;
+    public final int PARENTESIS_IZQ=14;
+    public final int PARENTESIS_DER=15;
+    public final int LLAVE_IZQ=16;
+    public final int LLAVE_DER=17;
+    public final int IGUAL=18;
+    public final int IF=19;
+    public final int WHILE=20;
+    public final int RETURN=21;
+    public final int ELSE=22;
+    public final int $=23;
+    
+    
+}
